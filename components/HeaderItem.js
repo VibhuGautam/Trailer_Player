c4b14468e275d14dd56611f0835baf4a0b8946a4
VibhuGautam <vibhuparmar13@gmail.com>
2021-05-06 21:27:23 +0530

    Initial Setup

---
+++ components/HeaderItem.js
@@ -0,0 +1,10 @@
+function HeaderItem({Icon , title}) {
+    return (
+        <div>
+            <Icon class ='h-8' />
+            <p>{title}</p>
+        </div>
+    )
+}
+
+export default HeaderItem
