49c7d7c0afba6a84a52244c5dd40c193b7dfd7b2
VibhuGautam <vibhuparmar13@gmail.com>
2021-05-07 13:18:54 +0530

    API Fetched

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   mode: 'jit',
-  purge: ['./src/**/*.html', './src/**/*.js',],
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
@@ -8,5 +8,7 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [   
+    require('tailwind-scrollbar-hide')
+  ]
 }
